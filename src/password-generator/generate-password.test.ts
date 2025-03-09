5ad82195af435a08968dbf4d0728ae9273383649
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2025-03-08 16:44:10 -0800

    install happy-dom

--- src/password-generator/generate-password.test.ts
+++ src/password-generator/generate-password.test.ts
@@ -8,5 +8,3 @@ test("password has length of 24 characters when length is 24", () => {
 test("password has length of 0 characters when all character types are false", () => {
   expect(generatePassword(false, false, false, false, 24)).toHaveLength(0)
 })
-
-// test if slider controls number display
